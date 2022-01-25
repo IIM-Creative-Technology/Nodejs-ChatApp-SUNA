ec1440fbd400de9985054737c275656427c93e70
Ulrich NGAKUI NGAHA <57942205+ngakui@users.noreply.github.com>
2022-01-25 23:41:28 +0100

    Adding services for authentification

--- express/routes/users.js
+++ express/routes/users.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs')
+const userServices = require('../services/user_services.js')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.status(200).json(
-    {
-      msg : "Hello User"
-    }
-  );
+router.get('/all', (req, res, next) => {
+  userServices.getAll().then(
+      (user) => res.status(200).json(user)
+  ).catch(err => next(err))
 });
 
+/** Route to register an User */
+router.post('/register', (req, res, next) => {
+  const {password} = req.body
+  const salt = bcrypt.genSaltSync();
+  req.body.password = bcrypt.hashSync(password, salt);
+
+  userServices.register(req.body).then(
+      () => res.send('success')
+  ).catch(
+      err => next(err)
+  )
+});
+
+/** Route to Login an User */
+router.post('/login', (req, res, next) => {
+  const { username, password} = req.body;
+  userServices.login({ username, password})
+      .then(user => {
+          res.json(user)
+      }
+  ).catch(err => next(err))
+});
+
+/** Route to Get User by ID. */
+router.get('/:id', (req, res, next) => {
+  userServices.getById(req.params.id).then(
+      (user) => res.json(user)
+  ).catch(err => next(err))
+});
 module.exports = router;
