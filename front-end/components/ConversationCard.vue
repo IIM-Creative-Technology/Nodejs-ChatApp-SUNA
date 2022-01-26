af1ef94eb930d06677b3829a8134e69ae19eb184
Alexandre Caramel <caramel.alexandre@gmail.com>
2022-01-26 17:03:45 +0100

    ADD pages for conversation

---
+++ front-end/components/ConversationCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-white max-w-lg mx-auto p-4 md:p-12 mt-10 rounded-lg shadow-2xl">
+    <!-- <NuxtLink :to="{name:'conversation-name', params: {name: }}" title="Go to the conversation"> -->
+    <h2>User name</h2>
+    <p>Last message for the conversation</p>
+    <!-- </NuxtLink> -->
+  </div>
+</template>
+
+<script>
+
+</script>
+
