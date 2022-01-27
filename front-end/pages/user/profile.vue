89a839ddba4b205796a8cdaa43f49f874a9dd7d0
SamyMahmoudi <samy.mahmoudi@edu.devinci.fr>
2022-01-27 19:41:43 +0100

    Update profile done

--- front-end/pages/user/profile.vue
+++ front-end/pages/user/profile.vue
@@ -6,20 +6,20 @@
       <!-- username -->
       <div class="mb-6 pt-3 rounded bg-gray-200">
           <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="username">Edit username*</label>
-          <input type="text" id="username" v-model.trim="$auth.user.username"
+          <input type="text" id="username" v-model.trim="formProfile.username"
           class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
       </div><!-- username -->
 
       <!-- email -->
       <div class="mb-6 pt-3 rounded bg-gray-200">
           <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Edit email*</label>
-          <input type="email" id="email" v-model.trim="$auth.user.email"
+          <input type="email" id="email" v-model.trim="formProfile.email"
           class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
       </div><!-- email -->
       <!-- password -->
       <div class="mb-6 pt-3 rounded bg-gray-200">
-          <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Older Password*</label>
-          <input autocomplete="on" type="password" id="password"
+          <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Password*</label>
+          <input autocomplete="on" type="password" id="password" v-model.trim="formProfile.password"
           class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
       </div><!-- password -->
 
@@ -41,7 +41,7 @@
       <!-- password -->
       <div class="mb-6 pt-3 rounded bg-gray-200">
           <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="password">Older Password*</label>
-          <input autocomplete="on" type="password" id="password" v-model.trim="formPassword.currentPassword"
+          <input autocomplete="on" type="password" id="password" v-model.trim="formPassword.password"
           class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3">
       </div><!-- password -->
       <!-- password -->
@@ -77,14 +77,14 @@ export default {
   data() {
     return {
       formProfile : {
-        username : null,
-        email : null,
+        username : this.$auth.user.username,
+        email : this.$auth.user.email,
         password : null,
         error : null,
         success : null
       },
       formPassword : {
-        currentPassword : null,
+        password : null,
         newPassword : null,
         confirmNewPassword : null,
         error : null,
@@ -96,22 +96,21 @@ export default {
     // update profile
     UpdateProfile() {
       this.formProfile.error = null
-
       // no fields empty
       if (!this.formProfile.username || !this.formProfile.email || !this.formProfile.password) return (this.formProfile.error = 'All fields are necessary')
-
+      const token = this.$auth.user._id
       this.$axios
-        .post(`/api/ROUTE`, {
-          token : this.$auth.user._id,
+        .put(`/api/users/user-update`, {
           username: this.formProfile.username,
           email: this.formProfile.email,
           password: this.formProfile.password,
+          headers: { Authorization: `Bearer ${token}` }
         })
         .then(() => {
-          this.formProfile.success = true
+          this.formProfile.success = "Update done"
         })
         .catch((error) => {
-          this.formProfile.error = error
+          this.formProfile.error = "An error occured"
         })
     },
 
@@ -120,21 +119,22 @@ export default {
        this.formPassword.error = null
 
       // no fields empty
-      if (!this.formPassword.currentPassword || !this.formPassword.newPassword || !this.formPassword.confirmNewPassword) return (this.formPassword.error = 'All fields are necessary')
+      if (!this.formPassword.password || !this.formPassword.newPassword || !this.formPassword.confirmNewPassword) return (this.formPassword.error = 'All fields are necessary')
 
       if(this.formPassword.newPassword != !this.formPassword.confirmNewPassword) return (this.formPassword.error = 'The new password and its confirmation are different')
 
       this.$axios
-        .post(`/api/ROUTE`, {
-          token : this.$auth.user._id,
-          password: this.formPassword.currentPassword,
+        .put(`/api/users/user-update`, {
+          password: this.formPassword.password,
           newPassword: this.formPassword.newPassword,
+
+          headers: { Authorization: `Bearer ${token}` }
         })
         .then(() => {
-          this.formPassword.success = true
+          this.formPassword.success = "Update done"
         })
         .catch((error) => {
-          this.formPassword.error = error
+          this.formPassword.error = "An error occured"
         })
     }
   }
