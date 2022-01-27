a16c519afe811c2388d4d657be50eb70b6116ae4
nathanszig <nathan.szigeti@edu.devinci.fr>
2022-01-28 00:28:24 +0100

    FIX message get

--- express/services/message_services.js
+++ express/services/message_services.js
@@ -7,7 +7,7 @@ async function addMessage(params){
     await message.save();
 }
 async function getMessage(params){
-    console.log(params)
+    console.log(params.id)
     // instantiate a user modal and save to mongoDB
     const message = await Message.find({_idConv: params.id}).populate({
         path: '_idConv',
@@ -21,12 +21,23 @@ async function getMessage(params){
         }
     ]
     }).populate('_idUser1')
-    console.log('message')
-    console.log(message[0].message)
+    console.log(message)
+    return message
+}
+
+async function deleteMessage(params){
+    const message = await Message.find({_id: params.id})
+    await message.delete()
+}
+
+async function getAll(){
+    const message = await Message.find()
     return message
 }
 
 module.exports = {
     addMessage,
-    getMessage
+    getMessage,
+    deleteMessage,
+    getAll
 };
